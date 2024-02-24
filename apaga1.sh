9230a326968d6a9410b844b9de8906d4f01658c4
joralgra <joralgra.dev@gmail.com>
2024-02-24 11:49:39 +0100

    Added /bin/bash

--- apaga1.sh
+++ apaga1.sh
@@ -1,4 +1,3 @@
-PDU=pdu1-labs.disca.upv.es
-USER=practica
-PASS=cos
+#!/bin/bash
+
 { printf "$USER\n$PASS\n1\n2\n2\nyes\n\n\e\e4"; sleep 2;}|telnet $PDU
--- apaga_todas.sh
+++ apaga_todas.sh
@@ -1,6 +1,5 @@
-PDU=pdu1-labs.disca.upv.es
-USER=practica
-PASS=cos
+#!/bin/bash
+
 for i in 1 2 3 4 5 6 
 do
 	{printf "$USER\n$PASS\n1\n$i\n2\nyes\n\n\e\e4"; sleep 2;}|telnet $PDU
